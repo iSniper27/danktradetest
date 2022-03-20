26c84c6ba2b6a0a83b10ca719855016c39428ecf
iSniper27 <bosdaan11@gmail.com>
2022-03-20 23:19:27 +0100

    update

--- src/cmds/trade.py
+++ src/cmds/trade.py
@@ -2,7 +2,7 @@ from discord.message import send_message, retreive_message
 from discord.button import interact_button
 from time import sleep
 
-def trade(token, user_id, session_id, trader_info, item):
+def trade_item(token, user_id, session_id, trader_info, item):
     channel_id = trader_info[3]
 
     send_message(channel_id, token, f"pls shop {item}")
@@ -23,7 +23,34 @@ def trade(token, user_id, session_id, trader_info, item):
 
     interact_button(channel_id, token, latest_message["components"][-1]["components"][-1]["custom_id"], latest_message, session_id)
 
-    sleep(1)
+    sleep(0.3)
+
+    latest_message = retreive_message(channel_id, trader_info[2], trader_info[0])
+
+    if latest_message is None:
+        return
+
+    interact_button(channel_id, trader_info[2], latest_message["components"][0]["components"][-1]["custom_id"], latest_message, trader_info[1])
+
+def trade_wallet(token, user_id, session_id, trader_info):
+    channel_id = trader_info[3]
+
+    send_message(channel_id, token, "pls bal")
+
+    latest_message = retreive_message(channel_id, token, user_id)
+
+    total = latest_message["embeds"][-1]["description"].split("\n")[0][14:].replace(",", "")
+
+    send_message(channel_id, token, f"pls trade {int(total)*(1/1.03)} {trader_info[0]}")
+
+    latest_message = retreive_message(channel_id, token, user_id)
+
+    if latest_message is None:
+        return
+
+    interact_button(channel_id, token, latest_message["components"][-1]["components"][-1]["custom_id"], latest_message, session_id)
+
+    sleep(0.3)
 
     latest_message = retreive_message(channel_id, trader_info[2], trader_info[0])
 
--- src/main.py
+++ src/main.py
@@ -12,27 +12,42 @@ cwd = f"{cwd}/" if cwd != "" else cwd
 from threading import Thread
 from discord.message import send_message
 from configuration.credentials import load_credentials, load_trader
-from cmds.trade import trade
+from cmds.trade import trade_item, trade_wallet
 
 credentials = load_credentials(cwd)
 
-print("command list:\n1)trade\n")
-command = int(input("what command?\n>"))
+while True:
+	print("command list:\n1)trade item\n2)trade wallet\n")
+	command = int(input("what command?\n>"))
 
-if command == 1:
-	trader_info = load_trader(cwd)
-	item = input("What item?")
+	if command == 1:
+		trader_info = load_trader(cwd)
+		item = input("What item?")
 
-	for index in range(len(credentials)):
-		user_id = credentials[index][0]
-		session_id = credentials[index][1]
-		token = credentials[index][2]
-		while True:
-			try:
-				sleep(0.3)
-				trade(token, user_id, session_id, trader_info, item)
-				break
-			except:
-				continue
-			else:
-				break
+		for index in range(len(credentials)):
+			user_id = credentials[index][0]
+			session_id = credentials[index][1]
+			token = credentials[index][2]
+			while True:
+				try:
+					sleep(0.3)
+					trade_item(token, user_id, session_id, trader_info, item)
+					break
+				except:
+					continue
+	
+	if command == 2:
+		trader_info = load_trader(cwd)
+
+		for index in range(len(credentials)):
+			user_id = credentials[index][0]
+			session_id = credentials[index][1]
+			token = credentials[index][2]
+			while True:
+				try:
+					sleep(0.3)
+					trade_wallet(token, user_id, session_id, trader_info)
+					break
+				except Exception as e:
+					print(e)
+					continue
